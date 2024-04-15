61a1e1df960bf8f3dec0c024ef7e49a2633b28ae
Isuru <isurumbandara@gmail.com>
2024-04-15 09:40:10 +0530

    details add

--- App.tsx
+++ App.tsx
@@ -7,6 +7,24 @@ const App = () => {
       <View style={styles.topicContainer}>
         <Text style={styles.topic}>GPA ESTIMATOR</Text>
       </View>
+      <View style={styles.firstSentContainer}>
+        <Text style={styles.firstSentence}>
+          You can enter your results and see GPA accourding to your degree
+          programm
+        </Text>
+      </View>
+      <View style={styles.yearContainer}>
+        <Text style={styles.yearText}>For first year(1)</Text>
+      </View>
+      <View style={styles.yearContainer}>
+        <Text style={styles.yearText}>For second year(2)</Text>
+      </View>
+      <View style={styles.yearContainer}>
+        <Text style={styles.yearText}>For third year(3)</Text>
+      </View>
+      <View style={styles.yearContainer}>
+        <Text style={styles.yearText}>For forth year(4)</Text>
+      </View>
     </View>
   );
 };
@@ -31,7 +49,27 @@ const styles = StyleSheet.create({
     fontSize: 26,
     fontWeight: 'bold',
   },
-
+  firstSentContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  firstSentence: {
+    fontSize: 20,
+    fontWeight: '300',
+    textAlign: 'center',
+  },
+  yearContainer: {
+    marginTop: 5,
+    marginBottom: 10,
+    marginLeft: 10,
+    alignItems: 'center',
+  },
+  yearText: {
+    fontSize: 20,
+    fontStyle: 'italic',
+  },
 });
 
 export default App;
